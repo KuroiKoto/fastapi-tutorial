922ec0c06c58c5ae0b72c1013814d613b3769bb6
KuroiKoto <vincent.mezino+kuroikoto@gmail.com>
2021-07-26 02:04:09 +0200

    [UserGuide] Query Parameters - https://fastapi.tiangolo.com/tutorial/query-params/#query-parameters

--- main.py
+++ main.py
@@ -11,10 +11,17 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 
 @app.get("/items/{item_id}")
